1ed81a2a4f8a11b38d2112f13955af85a89ebee3
thangduc <tdthang.it@gmail.com>
2023-09-30 11:15:54 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -8,10 +9,17 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.post("/telegram", (req, res) => { 
+app.post("/telegram", (req, res) => {
   console.log(req.body);
-  res.send("ok");
+  const arr = Object.keys(req.body)[0].split(",");
+  const webhook = arr[0];
+  const stt = arr[1];
+  const name = arr[2];
+  const remaining = arr[3];
+  axios.post(webhook, {
+    text: `Tiền của *${name}* vừa bị Đức Anh lấy, còn lại ${remaining} cá`,
+    sheet: arr,
+  });
 });
 
 const PORT = process.env.PORT;
