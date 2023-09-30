10a969684889132811916f5a7170ce134c19c27b
thangduc <tdthang.it@gmail.com>
2023-09-30 11:30:17 +0700

    update

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/telegram", (req, res) => {
-  console.log(req.body);
   const arr = Object.keys(req.body)[0].split(",");
   const webhook = arr[0];
   const stt = arr[1];
   const name = arr[2];
   const remaining = arr[3];
   axios.post(webhook, {
-    text: `Tiền của *${name}* vừa bị Đức Anh lấy, còn lại ${remaining} cá`,
+    text: `Tiền của ${name} còn lại ${remaining} cá`,
     sheet: arr,
   });
 });
