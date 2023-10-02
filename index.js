e1133ffcab6f0c646666294e801f9541ba6699cb
thangduc <tdthang.it@gmail.com>
2023-10-02 09:43:45 +0700

    update

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/telegram", (req, res) => {
-  const arr = Object.keys(req.body)[0].split(",");
-  const webhook = arr[0];
-  const stt = arr[1];
-  const name = arr[2];
-  const remaining = arr[3];
-  if (!webhook) return res.status(400).json({ message: "Webhook is required" });
-  axios.post(webhook, {
-    text: `Tiền của ${name} còn lại ${remaining} cá`,
-    sheet: arr,
-  });
+app.post("/telegram", async (req, res) => {
+  try {
+    const arr = Object.keys(req.body)[0].split(",");
+    const webhook = arr[0];
+    const stt = arr[1];
+    const name = arr[2];
+    const remaining = arr[3];
+    if (!webhook) return res.status(400).json({ message: "Webhook is required" });
+    await axios.post(webhook, {
+      text: `Tiền của ${name} còn lại **${remaining}** cá`,
+      sheet: arr,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 const PORT = process.env.PORT;
